43d9b96a0f459eeece50b498535bf8feadb0122d
Simpan1001 <simon.springer@edu.huddinge.se>
2020-01-19 14:27:47 +0100

    Added the second page with more sounds

---
+++ server.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let app = express();
+let port = process.env.PORT || 2345;
+
+app.use(express.static(__dirname + '/Public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/Public/index.html');
+});
+
+app.get('/sida2', function(req, res) {
+    res.sendFile(__dirname + '/Public/index2.html');
+});
+
+let server = app.listen(port, function() {
+    console.log(`The server is up and tunning on port ${port}`);
+});
